b3e097087655e5da455bccec55ab0a5b938f968c
kaspe <kaspe@DESKTOP-7ME4MUI>
2020-11-18 14:52:35 +0100

    updated script

--- src/DB_script.java
+++ src/DB_script.java
@@ -8,3 +8,27 @@ CREATE TABLE Customer
 		phoneno    varchar(16),
 		PRIMARY KEY (id)
 		on update cascade);
+
+
+
+CREATE TABLE SaleOrder   
+(id		int		IDENDITY(1,1)		not null,
+		date       DATETIME not null,
+		quantity       int not null,
+		deliveryStatus         varchar(15) not null,
+		deliveryDate       DATETIME,
+		deliveryNote     varchar(300),
+		orderDate    DATETIME not null,
+		customer_id     int,
+		PRIMARY KEY (id),
+		foreign key (Custumor_id) references Customer(id)
+		on update cascade);
+
+
+
+CREATE TABLE SaleOrderLine   
+(id		int		IDENDITY(1,1)		not null,
+		quantity       int not null,
+		saleorder_id         int not null,
+		PRIMARY KEY (id),
+		foreign key (saleorder_id) references SaleOrder(id));
