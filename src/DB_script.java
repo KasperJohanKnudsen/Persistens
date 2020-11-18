d1a227139204f300aa0676d18d5dc961c027129a
kaspe <kaspe@DESKTOP-7ME4MUI>
2020-11-18 15:47:28 +0100

    DB_script_final_maybe lol

--- src/DB_script.java
+++ src/DB_script.java
@@ -32,3 +32,18 @@ CREATE TABLE SaleOrderLine
 		saleorder_id         int not null,
 		PRIMARY KEY (id),
 		foreign key (saleorder_id) references SaleOrder(id));
+
+
+
+CREATE TABLE Product   
+(id		int		IDENDITY(1,1)		not null,
+		name       varchar(100) not null,
+		barcode       int not null,
+		purchasePrice       decimal not null,
+		salesPrice       decimal not null,
+		origin       varchar(25),
+		stock       int not null,
+		size       varchar(20),
+		description       varchar(300),
+		saleorderline_id         int not null,
+		foreign key (saleorderline_id) references SaleOrderLine(SaleOrderLine));
